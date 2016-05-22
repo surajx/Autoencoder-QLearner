c57df7759b60bf5d48ff40514b4b39bc32867ac3
Suraj Narayanan S <surajx@gmail.com>
2016-05-22 14:41:15 +1000

    code for plotting weight images

--- code/autoenc.py
+++ code/autoenc.py
@@ -160,4 +160,92 @@ def predict(encoder, state):
     return tuple(list(np.round(normalize(state_encoded)[0])))
 
 if __name__ == '__main__':
-    train(hidden=[50, 20])
+    encoder, autoencoder = train(
+        train_data='state_file_mediumClassic_uniq.dat', epoch=3000, hidden=[10])
+
+    # print(encoder.get_weights())
+    # for wt in encoder.get_weights():
+    #     for w in wt:
+    #         print("lskjf")
+    #         print(w)
+
+    wt = encoder.get_weights()
+    # ip_w = wt[2]
+    # for layer in [wt[0].T, wt[2].T]
+    #     for w in layer:
+    #         w.reshape((7, 8))
+
+    import matplotlib.pyplot as plt
+    # # use Matplotlib (don't ask)
+    cnt = 0
+    for layer in [wt[0].T, wt[2].T]:
+        plt.figure(figsize=(30, 4))
+        n = len(layer)
+        for i, w in enumerate(layer):
+            ax = plt.subplot(2, n, i + 1)
+            plt.imshow(w.reshape((7, 8)))
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+        plt.savefig('filename_w_hidden_classic' + str(cnt) + '.svg')
+        cnt += 1
+
+    # fig = plt.figure(figsize=(20, 4))
+    # wt = encoder.get_weights()
+    # ax = fig.add_subplot(121)
+    # ax.imshow(wt[0])
+    # ax.get_xaxis().set_visible(False)
+    # ax.get_yaxis().set_visible(False)
+
+    # # display reconstruction
+    # ax = fig.add_subplot(122)
+    # ax.imshow(wt[2])
+    # ax.get_xaxis().set_visible(False)
+    # ax.get_yaxis().set_visible(False)
+
+    # plt.savefig('filename_50.svg')
+
+    # # from keras.utils.visualize_util import plot
+    # # plot(autoencoder, to_file='model.png')
+
+    # get saved possible states and encode them.
+    # dataset = []
+    # with open('tmp_state_ip.dat', 'r') as orig:
+    #     rows = []
+    #     for l in orig:
+    #         if l == '\n':
+    #             dataset.append(rows)
+    #             rows = []
+    #             continue
+    #         row = []
+    #         for c in list(l.rstrip('\n')):
+    #             row.append(encode(c))
+    #         rows.append(row)
+
+    # x_train = np.array(dataset)
+    # x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    # x_train_norm = normalize(x_train)
+
+    # norm_st = list(np.round(normalize(autoencoder.predict(x_train_norm))))
+
+    # def decode(c):
+    #     if c >= 50 and c < 80:
+    #         return '%'
+    #     elif c >= 80 and c < 150:
+    #         return ' '
+    #     elif c >= 150 and c < 300:
+    #         return '.'
+    #     elif c >= 300 and c < 350:
+    #         return 'G'
+    #     elif c >= 350 and c <= 400:
+    #         return '>'
+
+    # # norm_st_enc.reverse()
+
+    # with open('tmp.dat', 'w') as f:
+    #     for norm_st_enc in norm_st:
+    #         for idx, c in enumerate(norm_st_enc):
+    #             if (idx + 1) % 8 == 0:
+    #                 f.write(decode(int(c)) + '\n')
+    #             else:
+    #                 f.write(decode(int(c)))
+    #         f.write('\n')
